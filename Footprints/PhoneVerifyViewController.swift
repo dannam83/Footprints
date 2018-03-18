cefb70f0bd362afd23b06c0ff73a46cd61cf8720
Daniel Nam <dannam83@gmail.com>
2018-03-18 19:47:42 -0400

    check for repeat use of phone number

--- Footprints/PhoneVerifyViewController.swift
+++ Footprints/PhoneVerifyViewController.swift
@@ -14,6 +14,7 @@ import FirebaseDatabase
 class PhoneVerifyViewController: UIViewController {
 
     @IBOutlet weak var phoneTF: UITextField!
+    var usedBy: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,32 +27,35 @@ class PhoneVerifyViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
     @IBAction func phoneSubmit(_ sender: Any) {
         guard let phone = phoneTF.text, phone != "" else {
             AlertController.showAlert(self, title: "Phone error", message: "Phone number not entered")
             return
         }
         let phoneMod = phone.replacingOccurrences(of: "-", with: "")
-        let phoneUser = self.phoneUser(phone: phoneMod)
-       
-        guard phoneUser == nil else {
+        self.phoneUser(phone: phoneMod)
+        self.phoneCheck(phone: phoneMod)
+        let userUID = Auth.auth().currentUser!.uid
+        guard usedBy == nil else {
             AlertController.showAlert(self, title: "Phone error", message: "Phone number is already being used. Please contact us if this is an error.")
             return
         }
-        let userUID = Auth.auth().currentUser!.uid
         self.phoneSave(phone: phoneMod, userID: userUID)
         self.performSegue(withIdentifier: "phoneVerifiedSegue", sender: nil)
     }
     
-        func phoneUser(phone: String) -> String? {
-            print("in phone check")
+        func phoneCheck(phone: String) {
+            guard phone.count == 10 else {
+                AlertController.showAlert(self, title: "Phone error", message: "Phone number must be 10 digits.")
+                return
+            }
+        }
+    
+        func phoneUser(phone: String) {
             let dbPhones = DatabaseAPI.shared.phonesReference
-            var usedBy: String?
             dbPhones.child(String(phone)).observe(DataEventType.value, with: {(snapshot) in
-                usedBy = snapshot.value as? String
+                self.usedBy = snapshot.value as? String
             })
-            return usedBy
         }
     
         func phoneSave(phone: String, userID: String) {
